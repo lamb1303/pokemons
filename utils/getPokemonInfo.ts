3b987473e1e9564178bd104db7d19adafe4eaa7d
luis <luis@luiss-MacBook-Pro.local>
2022-06-15 01:10:39 -0500

    Feature: Redirect based on the response

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -1,10 +1,14 @@
 import pokeApi from "../api/pokeApi";
 import { PokemonListFull } from "../interfaces";
 export const getPokemonInfo = async (nameOrId: string) => {
-  const { data } = await pokeApi.get<PokemonListFull>(`/pokemon/${nameOrId}`);
-  return {
-    id: data.id,
-    sprites: data.sprites,
-    name: data.name,
-  };
+  try {
+    const { data } = await pokeApi.get<PokemonListFull>(`/pokemon/${nameOrId}`);
+    return {
+      id: data.id,
+      sprites: data.sprites,
+      name: data.name,
+    };
+  } catch (error) {
+    return null;
+  }
 };
